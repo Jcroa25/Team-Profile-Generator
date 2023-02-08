0174c742cc36377aca581a4697c56d42f9c7461f
JuanCarlos Roa Jr <roajrjuancarlos@hotmail.com>
2023-02-08 15:01:33 -0500

    added markdown + index.js code

---
+++ index.js
@@ -0,0 +1,129 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateHTML = require('./src/generateHTML');
+const Employee = require('./lib/Employee');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
+const teamArray = [];
+const getManager = () => {
+    return inquirer.prompt([
+        {
+         type: 'input',
+         name: 'name',
+         message: 'Enter name of team Manager'
+        },
+        {
+         type: 'input',
+         name: 'id',
+         message: 'Please enter Manager ID'
+        },
+        {
+         type: 'input',
+         name: 'email',
+         message: 'Please enter managers email'
+        },
+        {
+         type: 'input',
+         name: 'officeNumber',
+         message: 'Please enter managers office phone number'
+        }
+    ])
+        .then((managerInput) => {
+            const { name, id, email, officeNumber } = managerInput;
+            const manager = new Manager(name, id, email, officeNumber);
+            teamArray.push(manager);
+            getEmployee();
+        })
+        .catch((err) => console.error(err));
+};
+
+const getEmployee = () => {
+    return inquirer.prompt([
+        {
+         type: 'list',
+         name: 'role',
+         message: 'Add Employee',
+         choices: ['Engineer', 'Intern', 'No More Employees']
+        },
+    ])
+    .then((employeeInput) => {
+        switch (employeeInput.role) {
+          case 'Engineer':
+           return getEngineer();
+          case 'Intern':
+           return getIntern();
+          case 'No More Employees':
+           return fs.writeFile('./dist/index.html', generateHTML(teamArray), (err) =>
+            err ? console.log(err) : console.log('Successfully created index.html!')
+         );
+        }
+    })
+    .catch((err) => console.error(err));
+
+};
+
+const getEngineer = () => {
+    return inquirer.prompt([
+        {
+         type: 'input',
+         name: 'name',
+         message: 'Please enter employee name'
+        },
+        {
+         type: 'input',
+         name: 'id',
+         message: 'Please enter their employee ID'
+        },
+        {
+         type: 'input',
+         name: 'email',
+         message: 'Please enter their email'
+        },
+        {
+         type: 'input',
+         name: 'github',
+         message: 'Please enter employee github username',
+        },
+    ])
+        .then((engineerInput) => {
+            const { name, id, email, github } = engineerInput;
+            const engineer = new Engineer(name, id, email, github);
+            teamArray.push(engineer);
+            getEmployee();
+        })
+        .catch((err) => console.error(err));
+}
+
+const getIntern = () => {
+    return inquirer.prompt([
+        {
+         type: 'input',
+         name: 'name',
+         message: 'Please enter employee name'
+        },
+        {
+         type: 'input',
+         name: 'id',
+         message: 'Please enter their employee ID'
+        },
+        {
+         type: 'input',
+         name: 'email',
+         message: 'Please enter their email'
+        },
+        {
+         type: 'input',
+         name: 'school',
+         message: "Enter the school the intern attends",
+        },
+    ])
+        .then((internInput) => {
+            const { name, id, email, school } = internInput;
+            const intern = new Intern(name, id, email, school);
+            teamArray.push(intern);
+            getEmployee();
+        })
+        .catch((err) => console.error(err));
+}
+getManager()
---
+++ src/generateHTML.js
@@ -0,0 +1,90 @@
+function generateEngineerCard(engineer) {
+    return `
+     <div class="col">
+      <div class="card">
+       <div class="card-body">
+         <h5 class="card-title">${engineer.name}</h5>
+          <h6 class="card-subtitle mb-2 text-muted">${engineer.getRole()}</h6>
+            <p class="card-text">ID: ${engineer.id}</p>
+            <p>Github: <a href="https://github.com/${engineer.github}" class="card-link">${engineer.github}</a> </p>
+            <p class="card-text">Email: <a href="mailto:${engineer.email}" class="card-link">${engineer.email}</a> </p>                 
+       </div>
+      </div>
+     </div>
+         `
+ }
+
+ function generateInternCard(intern) {
+    return `
+    <div class="col">
+    <div class="card">
+     <div class="card-body">
+      <h5 class="card-title">${intern.name}</h5>
+      <h6 class="card-subtitle mb-2 text-muted">${intern.getRole()}</h6>
+        <p class="card-text">ID: ${intern.id}</p>
+        <p class="card-text">School: ${intern.school}</p>
+        <p class="card-text">Email: <a href="mailto:${intern.email}" class="card-link">${intern.email}</a> </p>                  
+      </div>
+     </div>
+    </div> `
+ }
+ 
+function generateManager(manager) {
+ return `
+  <div class="col">
+    <div class="card">
+     <div class="card-body">
+      <h5 class="card-title"> ${manager.name} </h5>
+         <h6 class="card-subtitle mb-2 text-muted">${manager.getRole()}</h6>
+            <p class="card-text">ID: ${manager.id}</p>
+            <p class="card-text">Office Phone: ${manager.officeNumber}</p>
+            <p class="card-text">Email: <a href="mailto:${manager.email}" class="card-link">${manager.email}</a></p>                   
+     </div>
+    </div>
+  </div> `
+ }
+
+ function generateHTML(data) {
+    const teamcardsArray = [];
+    for (var i=0; i < data.length; i++ ) {
+       const employee = data[i];
+       const role = employee.getRole();
+       if (role === 'Manager') {
+          const managerCard = generateManager(employee);
+          teamcardsArray.push(managerCard);
+       }
+       if (role === 'Intern') {
+          const internCard = generateInternCard(employee);
+          teamcardsArray.push(internCard);
+       }
+       if (role === 'Engineer') {
+          const engineerCard = generateEngineerCard(employee);
+          teamcardsArray.push(engineerCard);
+       }
+    }
+    return `<!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="utf-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <title>Team Profile</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+        </head>
+        <body>
+        <nav class="navbar navbar-light" style="background-color: #FF0000" stylejustify-content-center>
+        <h1 class="navbar-brand" style="font-size: 40px;">Team A</h1>
+       </nav>
+        <div class="container mt-2">
+          <div class="row row-cols-1 row-cols-sm-3 g-4">
+            ${teamcardsArray.join(' ')}    
+            </div>
+            </div>      
+        </body>
+    </html>`
+ }
+
+ 
+
+ module.exports = generateHTML
